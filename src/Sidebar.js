0ad14962b4857ba8b38342223faef94afe717a41
Cogentx360 Labs <cogentx@Seans-MacBook-Pro.local>
2020-10-08 20:51:28 -0230

    Initialize project using Create React App w/ Redux

---
+++ src/Sidebar.js
@@ -0,0 +1,16 @@
+import './Sidebar.css';
+import React from 'react';
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h2>I am the Sidebar</h2>
+      <div className="sidebar__top">
+        <h3>Clever Programmer</h3>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Sidebar;
