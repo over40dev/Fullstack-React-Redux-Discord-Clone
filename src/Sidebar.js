da2d6f3678e8e2533ea83c02c5324bc8530a22ff
over40dev <ted@over40.dev>
2020-10-11 19:38:07 -0230

    Sidebar prototype complete

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,16 +1,69 @@
 import './Sidebar.css';
 import React from 'react';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
+import SidebarChannel from './SidebarChannel';
+import { Avatar } from '@material-ui/core';
+import SignalCellularAltIcon from '@material-ui/icons/SignalCellularAlt';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import CallIcon from '@material-ui/icons/Call';
+import MicIcon from '@material-ui/icons/Mic';
+import HeadsetIcon from '@material-ui/icons/Headset';
+import SettingsIcon from '@material-ui/icons/Settings';
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h2>I am the Sidebar</h2>
       <div className="sidebar__top">
-        <h3>Clever Programmer</h3>
+        <h3>SlangChat</h3>
+        <ExpandMoreIcon/>
+      </div>
+
+      <div className="sidebar__channels">
+        <div className="sidebar__channelsHeader">
+          <div className="sidebar__header">
+            <ExpandMoreIcon></ExpandMoreIcon>
+            <h4>Text Channels</h4>
+          </div>
+          <AddIcon className="sidebar__addChannel"/>
+        </div>
+
+        <div className="sidebar__channelsList">
+          <SidebarChannel/>
+          <SidebarChannel/>
+          <SidebarChannel/>
+          <SidebarChannel/>
+        </div>
+
       </div>
       
+      <div className="sidebar__voice">
+        <SignalCellularAltIcon className="sidebar__voiceIcon" fontSize="large"/>
+        <div className="sidebar__voiceInfo">
+          <h3>Voice Connected</h3>
+          <p>Stream</p>
+        </div>
+
+        <div className="sidebar__voiceIcons">
+          <InfoOutlinedIcon/>
+          <CallIcon/>
+        </div>
+      </div>
+
+      <div className="sidebar__profile">
+        <Avatar/>
+        <div className="sidebar__profileInfo">
+          <h3>@Cogentx360</h3>
+          <p>#thisIsMyID</p>
+        </div>
+        <div className="sidebar__profileIcons">
+          <MicIcon/>
+          <HeadsetIcon/>
+          <SettingsIcon/>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Sidebar;
